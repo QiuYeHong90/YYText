0dd393c34f4c9cf8ae0b9cb2eef9f983b08260ed
YeQiu <793983383@qq.com>
2024-09-13 13:41:45 +0800

    修复了系统输入法输入英文联想时的问题

--- Demo/YYTextDemo/AppDelegate.m
+++ Demo/YYTextDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//https://github.com/ibireme/YYText/issues/906
     return YES;
 }
 
