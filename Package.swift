108e8bd265e5a332434e3ae8f699fff6bcd16fbe
yeqiu <793983383@qq.com>
2026-05-11 18:01:23 +0800

    fx

--- Package.swift
+++ Package.swift
@@ -1,32 +1,22 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
     name: "YYText",
-    platforms: [
-        .iOS(.v13) // SPM 至少要 iOS 13，你要更低也可以用 .v9
-    ],
+    platforms: [.iOS(.v12)],
     products: [
-        .library(
-            name: "YYText",
-            targets: ["YYText"]
-        )
+        .library(name: "YYText", targets: ["YYText"])
     ],
     targets: [
         .target(
             name: "YYText",
             path: "YYText",
-            sources: ["."],
             publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath("."),
-                .define("SPM", to: "1")
-            ],
             linkerSettings: [
                 .linkedFramework("UIKit"),
-                .linkedFramework("CoreFoundation"),
                 .linkedFramework("CoreText"),
                 .linkedFramework("QuartzCore"),
+                .linkedFramework("CoreFoundation"),
                 .linkedFramework("Accelerate"),
                 .linkedFramework("MobileCoreServices"),
             ]
